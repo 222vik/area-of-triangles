4f0a2b857b839a651951fef9fcac24603950469e
222vik <viktor.pashkovsky7@gmail.com>
2018-06-20 19:49:36 +0300

    my_task

--- script.js
+++ script.js
@@ -1,56 +1,23 @@
-const pi=3.14;
-const r1=5;
-const r2=10;
-const r3=15;
+// Условие задачи: перевести температуру из форингейта в цельсий.
+// Снять показания в 8 час,12 час, 18 час, 24 час.
+const t_F_8=80;
+const t_F_12=105;
+const t_F_18=90;
+const t_F_24=72;
 
-s1=square(r1);
-function square(r) {
-  return 4*pi*r*r;
+const t_C_8=5/9*(t_F_8-32);
+const t_C_12=5/9*(t_F_12-32);
+const t_C_18=5/9*(t_F_18-32);
+const t_C_24=5/9*(t_F_24-32);
+// Через ф-ю это будет записываться так
+const t_C_8=F_to_C(t_F_8);
+const t_C_12=F_to_C(t_F_12);
+const t_C_18=F_to_C(t_F_18);
+const t_C_24=F_to_C(t_F_24);
+function F_to_C(T){
+  return (5/9)*(T-32)
 }
-console.log(s1);
-const a=1.4142;
-v1=a/12*5*5*5;
-// =====================
-//объем тетраэдра
-const x = Math.sqrt(2);
-console.log(x);
-v1=x/12*5*5*5;
-v2=x/12*10*10*10;
-console.log(v1);
-console.log(v2);
-// ===========================
-// объем тетраэдра через ф-ю
-const x = Math.sqrt(2);
-const a1=5;
-const a2=10;
-v2=cube(a2);
-  function cube(a){
-    return x/12*a*a*a;
-  }
-console.log(v2);
-// ==============================
-// площадь треугольника
-const a=5;
-const b=6;
-const c=7;
-const p1=(1/2)*(a+b+c);
-console.log(p1);
-// =====================================
-// площадь треугольника через ф-ю
-const a=5;
-const b=6;
-const c=7;
-let p=fom(5,6,7);
-function fom(a,b,c){
-  return (1/2)*(a+b+c);
-}
-console.log(p);
-var s = Math.sqrt(p*(p-a)*(p-b)*(p-c));
-console.log(s);
-
-var s=foo(9,5,6,7);
-function foo(p,a,b,c){
-  return Math.sqrt(p*(p-a)*(p-b)*(p-c));
-}
-console.log(s);
-// ==========================================
+    console.log (t_C_8);
+    console.log (t_C_12);
+    console.log (t_C_18);
+    console.log (t_C_24);
