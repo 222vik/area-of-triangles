8fb86f196a57ae9ca42047db532b44c5f47b8a27
222vik <viktor.pashkovsky7@gmail.com>
2018-06-20 20:17:53 +0300

    еще раз повторяем и еще раз копируем

--- script.js
+++ script.js
@@ -10,14 +10,26 @@ const t_C_12=5/9*(t_F_12-32);
 const t_C_18=5/9*(t_F_18-32);
 const t_C_24=5/9*(t_F_24-32);
 // Через ф-ю это будет записываться так
-const t_C_8=F_to_C(t_F_8);
-const t_C_12=F_to_C(t_F_12);
-const t_C_18=F_to_C(t_F_18);
-const t_C_24=F_to_C(t_F_24);
-function F_to_C(T){
-  return (5/9)*(T-32)
-}
-    console.log (t_C_8);
-    console.log (t_C_12);
-    console.log (t_C_18);
-    console.log (t_C_24);
+с
+// задача закончена
+// теперь через атом
+const t_F_8=80;
+const t_F_12=105;
+const t_F_18=90;
+const t_F_24=72;
+
+const t_C_8=5/9*(t_F_8-32);
+const t_C_12=5/9*(t_F_12-32);
+const t_C_18=5/9*(t_F_18-32);
+const t_C_24=5/9*(t_F_24-32);
+
+
+const t_F_8=80;
+const t_F_12=105;
+const t_F_18=90;
+const t_F_24=72;
+
+const t_C_8=5/9*(t_F_8-32);
+const t_C_12=5/9*(t_F_12-32);
+const t_C_18=5/9*(t_F_18-32);
+const t_C_24=5/9*(t_F_24-32);
